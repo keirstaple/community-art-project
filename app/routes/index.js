27c828cb34ea5115c633cc0bdf738449440a1f66
Keir Staple <keirstaple@comcast.net>
2016-05-16 15:44:26 -0700

    change name of event model to project to avoid confusing javascript

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return Ember.RSVP.hash({
+      project: this.store.findAll('event'),
+      comment: this.store.findAll('comment')
+    });
+  },
+
+  actions: {}
 });
--- tests/unit/models/project-test.js
+++ tests/unit/models/project-test.js
@@ -1,6 +1,6 @@
 import { moduleForModel, test } from 'ember-qunit';
 
-moduleForModel('event', 'Unit | Model | event', {
+moduleForModel('project', 'Unit | Model | project', {
   // Specify the other units that are required for this test.
   needs: []
 });
