51e2737c0b144de9709b0767c8ddaf99ae4a0ea6
chris <chrisberry321@yahoo.com>
2016-05-17 15:17:50 -0700

    changed a word on project-detail.js

--- app/components/project-detail.js
+++ app/components/project-detail.js
@@ -4,7 +4,7 @@ export default Ember.Component.extend({
   actions: {
   delete(project) {
     if (confirm('Are you sure you want to delete this project?')) {
-      this.sendAction('destroyProject', rental);
+      this.sendAction('destroyProject', project);
     }
   }
 }
