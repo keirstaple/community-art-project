d5499326ced56969cbd967e3b395e6f84071d5a2
chris <chrisberry321@yahoo.com>
2016-05-17 11:11:14 -0700

    contact us js and hbs and model

---
+++ app/components/contact-us.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewContact: false,
+  actions: {
+    contactFormShow() {
+      this.set('addNewContact', true);
+    },
+    save() {
+      var params = {
+        first: this.get('first'),
+        last: this.get('last'),
+        email: this.get('last'),
+        description: this.get('description'),
+      };
+      this.set('addNewContact', false);
+      this.sendAction('save', params);
+    }
+  }
+});
