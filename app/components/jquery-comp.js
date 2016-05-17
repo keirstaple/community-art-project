9cc582169207f257bf1a7b92697cc60eb9bd97ef
Brooke Hurford <brookebhurford@gmail.com>
2016-05-17 10:08:48 -0700

    Fix wonky issue when menu was closing

--- app/components/jquery-comp.js
+++ app/components/jquery-comp.js
@@ -4,10 +4,10 @@ export default Ember.Component.extend({
   actions: {
     menuPressed: function(){
       $('nav').addClass("mainNav-opened");
-      $("nav").removeClass("mainNav-closed");
+      $('nav').removeClass("mainNav-closed");
     },
     closeMenu: function(){
-      $("nav").addClass("mainNav-closed");
+      $('nav').addClass("mainNav-closed");
       $('nav').removeClass("mainNav-opened");
     }
   }
