ec5169042fb0e3bd4f6e525f8b8ee47a16d4d80e
Brooke Hurford <brookebhurford@gmail.com>
2016-05-17 09:28:07 -0700

    Add jquery component to show menu, sliding out is kind of janky

---
+++ app/components/jquery-comp.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    menuPressed: function(){
+      $('nav').addClass("mainNav-opened");
+      $("nav").removeClass("mainNav-closed");
+    },
+    closeMenu: function(){
+      $("nav").addClass("mainNav-closed");
+      $('nav').removeClass("mainNav-opened");
+    }
+  }
+});
