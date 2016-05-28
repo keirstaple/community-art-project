d03a3b786968c8276a05a91984105850bf98e0c2
Keir Staple <keirstaple@comcast.net>
2016-05-28 08:14:34 -0700

    edit fingerprint snippet to experiment

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     fingerprint: {
-      ignore: ['public/img']
+      ignore: ['img']
     }
     // Add options here
   });
