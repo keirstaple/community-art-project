12b4a3389778900d5d16cbe8805ef6772ec9b82a
Keir Staple <keirstaple@comcast.net>
2016-05-30 19:38:19 -0700

    move fingerprint ignore strings into same array

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     fingerprint: {
-      ignore: ['dist/img'], ['dist/assets']
+      ignore: ['dist/assets', 'dist/img']
     //   exclude: ['dist/assets']
     //   prepend: '../../public/img/'
     }
