944b56533f03353b4e7daff150ebd257daca667e
Keir Staple <keirstaple@comcast.net>
2016-05-30 20:22:55 -0700

    ignore fingerprinting in public folder

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,8 +5,8 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     fingerprint: {
-      // ignore: ['dist/assets', 'dist/img']
-      enabled: false
+      ignore: ['public']
+      // enabled: false
     }
     // Add options here
   });
